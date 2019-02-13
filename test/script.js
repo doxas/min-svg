5604d777567d1712217a27682bc0dbf0cfa7eb2b
doxas <eizanco@gmail.com>
2019-02-13 19:42:43 +0900

    update script.js

--- test/script.js
+++ test/script.js
@@ -2,7 +2,30 @@
 import util from '../index.js';
 
 window.addEventListener('load', () => {
-    let e = util.createNS('svg');
-    console.log(util.encodeHTML(e));
+    let svg = util.createNS('svg');
+    util.setAttribute(svg, {
+        fill: 'none',
+        stroke: 'red',
+        width: 200,
+        height: 200,
+    });
+    let path = util.createNS('path');
+    util.setAttribute(path, {
+        'stroke-width': 5,
+        d: 'M50,50L100,100,150,75Z',
+    });
+    svg.appendChild(path);
+
+    let uri = util.toDataURI(svg);
+    let i = new Image();
+    i.src = uri;
+    document.body.appendChild(i);
+    console.log(uri);
+
+    let b64 = util.toDataURI64(svg);
+    let j = new Image();
+    j.src = b64;
+    document.body.appendChild(j);
+    console.log(b64);
 }, false);
 
