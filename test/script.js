1b6ab4f1aa027b79548a737ee3e3206ee7577441
doxas <eizanco@gmail.com>
2019-02-14 14:52:08 +0900

    fix testscript

--- test/script.js
+++ test/script.js
@@ -2,21 +2,27 @@
 import util from '../index.js';
 
 window.addEventListener('load', () => {
+    // svg 要素など名前空間を必要とする要素の生成
     let svg = util.createNS('svg');
+    // setAttribute で属性を付与
     util.setAttribute(svg, {
         fill: 'none',
         stroke: 'red',
         width: 200,
         height: 200,
     });
+    // path 要素の d 属性値を作るためのオブジェクト
     let pathData = util.createPathData();
     pathData
     .moveTo(50, 50)
     .lineTo(100, 100, 150, 75)
+    .moveTo(150, 150)
+    .lineTo(150, 50, 175, 175)
     .closePath();
+
     let path = util.createNS('path');
     util.setAttribute(path, {
-        'stroke-width': 5,
+        'stroke-width': 3,
         d: pathData.d
     });
     svg.appendChild(path);
